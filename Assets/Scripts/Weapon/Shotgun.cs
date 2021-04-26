108cbdec9ee6c0ac8ff7a6e5865bf625e2eda5d3
Dirofu <if-sd@mail.ru>
2021-04-26 17:46:38 +0700

    Init

---
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Weapon
+{
+    [SerializeField] private float _delay;
+
+    private float _timeAfterShoot;
+
+    public override void Shoot(Transform shootPoint)
+    {
+        if (_timeAfterShoot < _delay)
+        {
+            Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+            _timeAfterShoot = _delay;
+        }
+        else
+        {
+            _timeAfterShoot -= Time.deltaTime;
+        }
+    }
+}
